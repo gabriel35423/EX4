a82713cfc93664d43a6d51755734d4a34a785f0a
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-11-12 19:10:20 -0300

    Add files via upload

---
+++ EX4WF-main/EX4WF/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace EX4WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(350, 138);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBase.TabIndex = 0;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(350, 224);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 1;
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.BackColor = System.Drawing.Color.Khaki;
+            this.lblBase.Location = new System.Drawing.Point(362, 122);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(72, 13);
+            this.lblBase.TabIndex = 2;
+            this.lblBase.Text = "Digite a base:";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.BackColor = System.Drawing.Color.Khaki;
+            this.lblAltura.Location = new System.Drawing.Point(362, 208);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(75, 13);
+            this.lblAltura.TabIndex = 3;
+            this.lblAltura.Text = "Digite a altura:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Khaki;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(265, 290);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(284, 48);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Location = new System.Drawing.Point(350, 370);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblBase);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtBase);
+            this.Name = "Form1";
+            this.Text = "Triangulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ EX4WF-main/EX4WF/Triangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4WF
+{
+    internal class Triangulo
+    {
+        private double baseTriangulo;
+
+        private double alturaTriangulo;
+
+
+
+        public Triangulo(double baseTriangulo, double alturaTriangulo)
+
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.alturaTriangulo = alturaTriangulo;
+        }
+
+        public Triangulo()
+
+        {
+            this.baseTriangulo = 0;
+            this.alturaTriangulo = 0;
+        }
+
+        public void setBase(double baseTriangulo)
+        { 
+            this.baseTriangulo = baseTriangulo; 
+        }
+
+        public void setAltura(double alturaTriangulo)
+        {
+            this.alturaTriangulo = alturaTriangulo;
+        }
+
+        public double getBase()
+        {
+            return baseTriangulo;
+        }
+
+        public double getAltura()
+        {
+            return alturaTriangulo;
+        }
+
+        public double CalcularArea()
+
+        {
+
+            return (baseTriangulo * alturaTriangulo) / 2;
+
+        }
+    }
+}
